fdf7d467e650ac87f2778991d35b5d4b8c14707a
Alexandre Santos <ale.santos49@gmail.com>
2021-03-12 15:53:54 -0300

    Initial commit

---
+++ Projetos Simples/FilmesCRUDRazor/FilmesCRUDRazor/Migrations/20210312160500_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmesCRUDRazor.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filme",
+                columns: table => new
+                {
+                    FilmeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    DataLancamento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Genero = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filme", x => x.FilmeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filme");
+        }
+    }
+}
---
+++ Projetos Simples/FilmesCRUDRazor/FilmesCRUDRazor/Models/Filmes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FilmesCRUDRazor.Models
+{
+    public class Filme
+    {
+        public int FilmeId { get; set; }
+        [Display(Name = "Título")]
+        [Required]
+        public string Titulo { get; set; }
+
+        [Display(Name = "Data de Lançamento")]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataLancamento { get; set; }
+
+        [Display(Name = "Gênero")]
+        [Required]
+        public string Genero { get; set; }
+
+        [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Preco { get; set; }
+    }
+}
