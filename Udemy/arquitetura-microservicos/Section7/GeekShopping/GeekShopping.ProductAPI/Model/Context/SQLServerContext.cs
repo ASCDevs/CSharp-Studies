516d192838c7768ad68af8a7ae0308d0b7021ca1
Alexandre Cavalcante <ale.santos49@gmail.com>
2022-10-29 12:34:58 -0300

    Adicionado código base da aplicação referente a aplicação de mricroserviços

---
+++ Udemy/arquitetura-microservicos/Section7/GeekShopping/GeekShopping.ProductAPI/Model/Context/SQLServerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SQLServerContext : DbContext
+    {
+        public SQLServerContext(){ }
+
+        public SQLServerContext(DbContextOptions<SQLServerContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set;}
+    }
+}
